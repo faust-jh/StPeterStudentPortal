3efebc01335cdbce8bcded6c458573c26c1e4d0c
Eliezer Thomas <rezeilesamoht@gmail.com>
2022-10-01 01:06:44 +0800

    Sign In and Sign up UI

--- app/src/main/java/com/example/capstone/Sign_In.java
+++ app/src/main/java/com/example/capstone/Sign_In.java
@@ -1,14 +1,120 @@
 package com.example.capstone;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Sign_In extends AppCompatActivity {
+
+    //DECLARATION
+    public String user, pass, usertype;
+    public String studUser = "Student", studPass = "Student";
+    public String facUser = "Faculty", facPass = "Faculty";
+    EditText txtUsername;
+    EditText txtPassword;
+    Spinner txtUsertype;
+    TextView signUp;
+    Button signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Spinner spinner = findViewById(R.id.Usertype);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.usertype, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+
+        //HOW TO CONNECT THE XML INTO THE VARIABLES
+        txtUsertype = (Spinner) findViewById(R.id.Usertype);
+        txtUsername = (EditText) findViewById(R.id.Usernamefield);
+        txtPassword = (EditText) findViewById(R.id.Passwordfield);
+        signUp = (TextView) findViewById(R.id.SignUp);
+        signIn = (Button) findViewById(R.id.Signbtn);
+
+
+        //FUNCTION FOR THE SIGNIN AND SIGNUP BUTTON\
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                user = txtUsername.getText().toString();
+                pass = txtPassword.getText().toString();
+                usertype = txtUsertype.getSelectedItem().toString();
+                SignIn();
+            }
+        });
+        signUp.setOnClickListener(view -> {
+            Intent Sign_Up = new Intent(this, Sign_Up.class);
+            startActivity(Sign_Up);
+        });
     }
+
+
+   void SignIn()
+   {
+
+        if(user.isEmpty())
+        {
+           Toast.makeText(getApplicationContext(), "Please enter username", LENGTH_LONG).show();
+           txtUsername.setError("Please enter username!");
+        }
+        else if(pass.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(), "Please enter password", LENGTH_LONG).show();
+            txtPassword.setError("Please enter Password!");
+        }
+        else if(user.isEmpty() && pass.isEmpty())
+        {
+           Toast.makeText(getApplicationContext(), "Please fill up the fields!", LENGTH_LONG).show();
+           txtUsername.setError("Please enter username!");
+           txtPassword.setError("Please enter Password!");
+        }
+        else
+        {
+            if (usertype.equals(""))
+            {
+                Toast.makeText(getApplicationContext(), "Please fill up the fields!", LENGTH_LONG).show();
+            }
+            else if (usertype.equals("Student")) {
+                if (user.equals(studUser) && pass.equals(studPass)) {
+                    Intent signIn = new Intent(this, StudentDashboard.class);
+                    startActivity(signIn);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Invalid Account!", LENGTH_LONG).show();
+                    txtUsername.setText("");
+                    txtPassword.setText("");
+                }
+            }
+            else if (usertype.equals("Faculty")) {
+                if (user.equals(facUser) && pass.equals(facPass)) {
+                    Intent signIn = new Intent(this, FacultyDashboard.class);
+                    startActivity(signIn);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Invalid Account!", LENGTH_LONG).show();
+                    txtUsername.setText("");
+                    txtPassword.setText("");
+                }
+
+            }
+        }
+   }
+
+
+
 }
--- app/src/main/java/com/example/capstone/Sign_Up.java
+++ app/src/main/java/com/example/capstone/Sign_Up.java
@@ -2,13 +2,116 @@ package com.example.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.internal.EdgeToEdgeUtils;
+
+import org.w3c.dom.Text;
 
 public class Sign_Up extends AppCompatActivity {
+
+    String fname, lname, email, username, pass, conpass;
+
+    EditText txtFname;
+    EditText txtLname;
+    EditText txtEmail;
+    EditText txtUsername;
+    EditText txtPass;
+    EditText txtConpass;
+    TextView txtTerms;
+    TextView txtSignIn;
+    Button btnSignUp;
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+
+    //FOR THE STUDENT ID POP UP VERIFICATION
+    private TextView enterStudID;
+    private EditText editStudID;
+    private Button enterStudIDbtn;
+
+    //FOR THE TERMS AND CONDITION POPUP
+    private TextView lblTermsandConditions;
+    private TextView txtTermsandConditions;
+    private Button doneBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //HOW TO CONNECT THE XML INTO THE VARIABLES
+        txtFname = (EditText) findViewById(R.id.FNamefield);
+        txtLname = (EditText) findViewById(R.id.LNamedfield);
+        txtEmail = (EditText) findViewById(R.id.MailField);
+        txtUsername = (EditText) findViewById(R.id.UsernameField);
+        txtPass = (EditText) findViewById(R.id.PasswordField);
+        txtConpass = (EditText) findViewById(R.id.ConfirmPassField);
+        txtTerms = (TextView) findViewById(R.id.TermsandCondition);
+        txtSignIn = (TextView) findViewById(R.id.createSignIn);
+        btnSignUp = (Button) findViewById(R.id.createSignUp);
+
+
+        //SHOULD BE CHANGED MUST BE TERMS AND CONDITION
+        txtTerms.setOnClickListener(view -> {
+            TermspopupDialog();
+        });
+        //FUNCTION FOR THE SIGNIN AND SIGNUP BUTTON
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                studIDpopupDialog();
+            }
+        });
+        txtSignIn.setOnClickListener(view -> {
+            Intent signIn= new Intent(this, Sign_In.class);
+            startActivity(signIn);
+        });
+
+    }
+
+    public void studIDpopupDialog() {
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View StudIDpopupView = getLayoutInflater().inflate(R.layout.studidpopup, null);
+        enterStudID = (TextView) StudIDpopupView.findViewById(R.id.enterStudID);
+        editStudID = (EditText) StudIDpopupView.findViewById(R.id.editStudID);
+        enterStudIDbtn = (Button) StudIDpopupView.findViewById(R.id.enterStudIDbtn);
+
+        dialogBuilder.setView(StudIDpopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        enterStudIDbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //define enter button
+            }
+        });
+    }
+
+    public void TermspopupDialog() {
+            dialogBuilder = new AlertDialog.Builder(this);
+            final View TermspopupView = getLayoutInflater().inflate(R.layout.termsandconditionpopup, null);
+            lblTermsandConditions = (TextView) TermspopupView.findViewById(R.id.lblTermsandConditions);
+            txtTermsandConditions = (TextView) TermspopupView.findViewById(R.id.txtTermsandConditions);
+            doneBtn = (Button) TermspopupView.findViewById(R.id.doneBtn);
+
+            dialogBuilder.setView(TermspopupView);
+            dialog = dialogBuilder.create();
+            dialog.show();
+
+                doneBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //define done button
+                    dialog.dismiss();
+                }
+            });
     }
 }
